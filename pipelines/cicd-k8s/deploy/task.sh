e94faa0e7501b1df153d38e296796c397807676b
Mevan Samaratunga <mevansam@gmail.com>
2018-10-29 02:19:06 +0400

    pass the version

--- pipelines/cicd-k8s/deploy/task.sh
+++ pipelines/cicd-k8s/deploy/task.sh
@@ -2,4 +2,30 @@
 
 set -xeuo pipefail
 
-echo "Deploying to $ENVIRONMENT..."
+export VERSION=$(cat version/version)
+echo "Deploying to spring-music verion $VERSION to $ENVIRONMENT."
+
+cat << ---EOF > deployment.yml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: spring-music-$ENVIRONMENT
+  labels:
+    app: spring-music
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: spring-music
+  template:
+    metadata:
+      labels:
+        app: spring-music
+    spec:
+      containers:
+      - name: spring-music
+        image: harbor.pcf.pcfenv1.pocs.pcfs.io/demo/spring-music:$VERSION
+        ports:
+        - containerPort: 8080
+---EOF
+
