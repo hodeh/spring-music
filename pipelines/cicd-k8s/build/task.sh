7d2e915c21467001e0b0b3af71d0f11bb2015e53
Mevan Samaratunga <mevansam@gmail.com>
2018-10-29 01:45:58 +0400

    updated build to allow versioning of jar

--- pipelines/cicd-k8s/build/task.sh
+++ pipelines/cicd-k8s/build/task.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-set -euo pipefail
+set -xeuo pipefail
 
-version=1.0
+export VERSION=$(cat version/version)
+cp version/version docker-build/version
 
-cd spring-music-repo
+cd spring-music
 ./gradlew clean assemble
 cd -
 
-mv spring-music-repo/build/libs/spring-music-${version}.jar docker-build
+ls -al spring-music/build/libs
+
+mv spring-music/build/libs/spring-music-${VERSION}.jar docker-build
 
 cat << ---EOF > docker-build/Dockerfile
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
-COPY spring-music-${version}.jar app.jar
+COPY spring-music-${VERSION}.jar app.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
 ---EOF
