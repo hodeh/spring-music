0fd0488c5c11227d01a636df2653761e740a55e6
hodeh <hodeh@pivotal.io>
2019-03-19 19:09:13 +0400

    standarize artifact version

--- pipelines/cicd-k8s/build/task.sh
+++ pipelines/cicd-k8s/build/task.sh
@@ -11,11 +11,11 @@ cd -
 
 ls -al spring-music/build/libs
 
-mv spring-music/build/libs/spring-music-${VERSION}.jar docker-build
+mv spring-music/build/libs/spring-music-1.0.jar docker-build
 
 cat << ---EOF > docker-build/Dockerfile
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
-COPY spring-music-${VERSION}.jar app.jar
+COPY spring-music-1.0.jar app.jar
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
 ---EOF
