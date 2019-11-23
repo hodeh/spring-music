39c8fe377b09c909fe25357b76a24389e81089da
hodeh <hodeh@pivotal.io>
2019-11-23 18:38:24 +0400

    update build task

--- pipelines/cicd-k8s/build/task.sh
+++ pipelines/cicd-k8s/build/task.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
 set -xeu
-
-export VERSION=$(cat version/version)
-cp version/version docker-build/version
-
 cd spring-music
-./gradlew clean assemble
-cd -
+./gradlew build
 
-ls -al spring-music/build/libs
-
-mv spring-music/build/libs/spring-music-1.0.jar docker-build
-
-cat << ---EOF > docker-build/Dockerfile
-FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-COPY spring-music-1.0.jar app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
----EOF
